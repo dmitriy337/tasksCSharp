6fc0b168b78b57952fcd7b60e399ae4a15e5c120
ᐯㄩ卄丨乙卂 ᐯㄩ卄 <47759698+dmitriy337@users.noreply.github.com>
2020-11-10 09:48:12 +0000

    Add files via upload

---
+++ FormatDouble/main.cs
@@ -0,0 +1,17 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            double numb = Double.Parse(Console.ReadLine());
+            Console.WriteLine("{0:f3}", numb);
+        }
+
+    }
+}
