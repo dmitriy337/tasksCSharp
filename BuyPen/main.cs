376699b091adfb1ef71229e53fad54981d677f5c
ᐯㄩ卄丨乙卂 ᐯㄩ卄 <47759698+dmitriy337@users.noreply.github.com>
2020-11-11 21:07:54 +0000

    Add files via upload

---
+++ BuyPen/main.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Ex1034
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] price = Console.ReadLine().Trim().Split();
+            string[] wowPrice = Console.ReadLine().Trim().Split();
+            // Расчёт в копейках
+            int pricePanInCent = int.Parse(price[0]) * 100 + int.Parse(price[1]);
+            int haveMoney = int.Parse(wowPrice[0]) * 100 + int.Parse(wowPrice[1]);
+            // 1-й вывод
+            Console.WriteLine(haveMoney/pricePanInCent);
+            // 2-й вывод
+            Console.WriteLine(haveMoney%pricePanInCent/100 + " " + haveMoney%pricePanInCent%100);
+        }
+    }
+}
