fbf5b6f2447375211c4c18ac7f1fc5c1abe8ec96
ᐯㄩ卄丨乙卂 ᐯㄩ卄 <47759698+dmitriy337@users.noreply.github.com>
2020-11-13 12:23:38 +0000

    Add files via upload

---
+++ ВосстановлениеДелимого/README.txt
@@ -0,0 +1,3 @@
+NumberDayOfWeek
+1 0  ->1 14
+9 8  -> 14 19
---
+++ ВосстановлениеДелимого/main.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string data = Console.ReadLine();
+            int FirtDay = Convert.ToInt32((data.Split(" "))[1]);
+            int Days = Convert.ToInt32((data.Split(" "))[0]);
+            int s = Days * 37 + FirtDay;
+            Console.WriteLine($"{s / 23} {s % 23}");
+
+        }
+    }
+}
